0e75cd64f957debadee6844b903f7f2221440d0a
JTsang95 <JTsang95@users.noreply.github.com>
2021-09-02 15:01:26 -0400

    generate JSON web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
 // @desc    Auth user & get token
@@ -9,13 +10,15 @@ const authUser = asyncHandler(async (req, res) => {
 
 	const user = await User.findOne({ email });
 
+	//check if user is in db and if password is valid
 	if (user && (await user.matchPassword(password))) {
+		//if valid user, generate a token with the user's id embedded inside
 		res.json({
 			_id: user._id,
 			name: user.name,
 			email: user.email,
 			isAdmin: user.isAdmin,
-			token: null,
+			token: generateToken(user._id),
 		});
 	} else {
 		res.status(401);
